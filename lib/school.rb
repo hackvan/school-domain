2cae579702e89fe292b53c4ae4bfad4eea1ae198
Diego Camacho <hackvan@gmail.com>
2018-07-06 21:00:19 -0500

    Initial commit

---
+++ lib/school.rb
@@ -0,0 +1,25 @@
+class School
+  attr_reader :name, :roster
+
+  def initialize(name)
+    @name   = name
+    @roster = {}
+  end
+
+  def add_student(name, grade)
+    @roster[grade] ||= []
+    @roster[grade] << name
+  end
+
+  def grade(grade)
+    return @roster[grade]
+  end
+
+  def sort
+    sorted_roster = {}
+    @roster.each do |grade, students|
+      sorted_roster[grade] = students.sort
+    end
+    return sorted_roster
+  end
+end
